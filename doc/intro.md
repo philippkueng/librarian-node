57cf91858a8ac962b2895707b9c497fca05c79ba
Philipp Küng <git@philippkueng.ch>
2015-02-24 12:35:45 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to librarian
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/librarian/core_test.clj
@@ -0,0 +1,7 @@
+(ns librarian.core-test
+  (:require [clojure.test :refer :all]
+            [librarian.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
