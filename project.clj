3b80115d0010a52eeb9b2b0fef69d8218ea9e51f
Philipp Küng <git@philippkueng.ch>
2015-02-26 21:01:47 +0100

    adding a url and a description to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject librarian "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A prototype for a unified search application across cloud services."
+  :url "https://github.com/philippkueng/librarian"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
