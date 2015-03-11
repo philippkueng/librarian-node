5d6e7b71c7d346846792880b84cc878f0da6b164
Philipp Küng <git@philippkueng.ch>
2015-03-11 08:12:30 +0100

    wrapping all environment variable inclusion into the env function

--- config.js
+++ config.js
@@ -6,20 +6,16 @@ var env = function (preferredValue, defaultValue) {
 
 exports.system = {
   host: 'localhost',
-  port: process.env.PORT ? process.env.PORT : 3000
-}
+  port: env(process.env.PORT, 3000)
+};
 
 exports.google = {
-  provider: 'google',
-  password: 'cookie-encryption-password',
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  provider: "google",
+  password: env(process.env.GOOGLE_COOKIE_PASSWORD, "cookie-encryption-password"),
+  clientId: env(process.env.GOOGLE_CLIENT_ID, ""),
+  clientSecret: env(process.env.GOOGLE_CLIENT_SECRET, ""),
   isSecure: false,
-  scope: [
-  'https://www.googleapis.com/auth/userinfo.email',
-  'https://www.googleapis.com/auth/drive.readonly',
-  'https://www.googleapis.com/auth/calendar.readonly',
-  'https://www.googleapis.com/auth/gmail.readonly'],
+  scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/gmail.readonly'],
   providerParams: {
     access_type: 'offline',
     response_type: 'code'
