abfafdba7622ff5bac7ba3cc459e9ad403403111
Philipp Küng <git@philippkueng.ch>
2015-03-16 21:46:17 +0100

    extend the id column to use a uuid instead of a serial

--- schema.sql
+++ schema.sql
@@ -1,6 +1,8 @@
+-- activate the uuid extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 DROP TABLE IF EXISTS "clients";
 CREATE TABLE "clients" (
-  "id" SERIAL,
-  "data" jsonb,
-  PRIMARY KEY ("id")
+  "id" uuid primary key default uuid_generate_v4(),
+  "data" jsonb
 );
